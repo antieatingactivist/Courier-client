0c3b7f7a47584771c61869aef1a5e8defc221aea
antieatingactivist <antieatingactivist@gmail.com>
2022-09-15 11:53:39 -0700

    created stops

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { StopListComponent } from './stop-list/stop-list.component';
 import { DetailsComponent } from './details/details.component';
+import { StopComponent } from './stop-list/stop/stop.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     StopListComponent,
-    DetailsComponent
+    DetailsComponent,
+    StopComponent
   ],
   imports: [
     BrowserModule,
