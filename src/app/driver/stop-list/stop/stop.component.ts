c18ed7483f4fba7e464bb230dd459ce6913b53f1
antieatingactivist <antieatingactivist@gmail.com>
2022-10-03 22:37:34 -0700

    time is now calculated to determine status color

--- src/app/driver/stop-list/stop/stop.component.ts
+++ src/app/driver/stop-list/stop/stop.component.ts
@@ -17,9 +17,41 @@ export class StopComponent implements OnInit {
 
 
   ngOnInit(): void {
-    console.log(new Date(this.stop.clientInfo.arrivalWindowEnd).toISOString(), new Date().toISOString());
+    
 
     this.status = this.stop.status;
+    if (this.status == "ready") {
+      this.status = this.determineEarlyOrLate();
+      
+    }
+  }
+
+  private determineEarlyOrLate() {
+    let status = "ready";
+    const currentTime = new Date().getTime();
+    const earliest = new Date(this.stop.clientInfo.arrivalWindowStart).getTime();
+    const latest = new Date(this.stop.clientInfo.arrivalWindowEnd).getTime();
+    console.log(
+       
+      currentTime - latest
+    );
+ 
+    if (earliest - currentTime > 0) {
+      status = "early";
+      if (earliest - currentTime < 60000 * 5) {
+        status = "almost-ready";
+      } 
+    }
+
+    if (currentTime - latest > -60000 * 5) {
+      status = "almost-late";
+      if (currentTime - latest > 0) {
+        status = "late";
+      }
+    }
+
+    return status;
+    
   }
 
 }
