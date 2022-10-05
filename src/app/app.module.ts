7fcd0f22805f681a082b264bb17d567db03437ba
antieatingactivist <antieatingactivist@gmail.com>
2022-10-05 13:31:53 -0700

    Revert "changed back icon"
    This reverts commit 9e8de29311bdf45d9cdd97a126a8425e63e318ac.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,8 @@ import { HttpClientModule } from "@angular/common/http";
 import { DispatchComponent } from './dispatch/dispatch.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatMenuModule} from '@angular/material/menu';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -21,7 +23,13 @@ import {MatMenuModule} from '@angular/material/menu';
     HttpClientModule,
     DriverModule,
     BrowserAnimationsModule,
-    MatMenuModule
+    MatMenuModule,
+    ServiceWorkerModule.register('ngsw-worker.js', {
+      enabled: environment.production,
+      // Register the ServiceWorker as soon as the application is stable
+      // or after 30 seconds (whichever comes first).
+      registrationStrategy: 'registerWhenStable:30000'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
