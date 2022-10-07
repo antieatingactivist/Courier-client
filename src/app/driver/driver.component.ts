322332c965c711416ae5879a0c1b1caded4cfaaa
antieatingactivist <antieatingactivist@gmail.com>
2022-10-06 21:06:54 -0700

    styled login

--- src/app/driver/driver.component.ts
+++ src/app/driver/driver.component.ts
@@ -18,12 +18,18 @@ export class DriverComponent implements OnInit {
   constructor(private dataService: DataService, private router: Router) {}
 
   ngOnInit() {
+    
+    
     this.dataService.getDrivers().subscribe({
       next: (data:any[]) => this.drivers = <any[]>data,
       error: (err :string) => {
         console.error(err),
         console.warn("Cannot connect to server, using Demo Driver Account");
         this.drivers = [{id: -1}];
+      },
+      complete: () => {
+        console.log(this.drivers)
+        this.driver.number = this.drivers[0].id;
       }
     });
   }
